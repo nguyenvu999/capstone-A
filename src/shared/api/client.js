dbc0f944f76379a48d5a9b7217ef2a53656b3585
nguyenvu999 <nguyenvu031103@gmail.com>
2026-05-18 04:55:17 +0700

    1

--- src/shared/api/client.js
+++ src/shared/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://backend-capstone-4f1a.onrender.com/api",
   withCredentials: true, // Quan trọng để gửi Cookie
 });
 
