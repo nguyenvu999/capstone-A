ab4c8d4d09850054379d63dcf05359f95ad42a8c
nguyenvu999 <nguyenvu031103@gmail.com>
2026-07-28 10:45:05 +0700

    Update FeatureFlags.js

--- frontend/src/shared/config/FeatureFlags.js
+++ frontend/src/shared/config/FeatureFlags.js
@@ -2,4 +2,4 @@
 
 // Chỉ bật khi ở LOCAL (import.meta.env.DEV) VÀ biến env = "true"
 export const ENABLE_EMAIL_PASSWORD_LOGIN =
-  import.meta.env.DEV && import.meta.env.VITE_ENABLE_EMAIL_PASSWORD_LOGIN === "false";
+  import.meta.env.DEV && import.meta.env.VITE_ENABLE_EMAIL_PASSWORD_LOGIN === "true";
